d1874ce579f770f212d977265b563e6553e8d3d6
hardik4555 <95064351+hardik4555@users.noreply.github.com>
2023-12-28 01:11:20 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const db= new pg.Client({
     // user: "postgres",
     host: "localhost",
     database: "perntodo",
-    password: "kittukhushi@17",
+    password: "**********",
     port: 5432,
 })
 
